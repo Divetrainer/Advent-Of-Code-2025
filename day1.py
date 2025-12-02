fc3dc062ebbfbdb9921f37230007235e0f16c2cf
Divetrainer <ian@divelab.tech>
2025-12-01 22:40:55 -0800

    part 2 completed!

--- day1.py
+++ day1.py
@@ -12,26 +12,32 @@ dial_start = 50
 dial_position = dial_start
 dial_movement = 0
 zero_counts = 0
+any_zero_count = 0
 for input in dial_inputs:
     if input[0] == "L":
         # print(f"Left {input[1:]}")
         dial_movement = int(input[1:])
         for i in range(1, dial_movement+1):
             dial_position -= 1
+            if dial_position == 0:
+                any_zero_count += 1
             if dial_position == -1:
                 dial_position = 99
+                
 
 
 
     elif input[0] == "R":
         #print(f"Right {input[1:]}")
         dial_movement = int(input[1:])
-        for i in range(dial_movement):
+        for i in range(1, dial_movement+1):
             dial_position += 1
             if dial_position == 100:
                 dial_position = 0
+                any_zero_count += 1
     
     if dial_position == 0:
         zero_counts += 1
 
 print(zero_counts)
+print(any_zero_count)
