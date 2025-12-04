be8d711b2e22ab9262b09bd7646b68ba4e02ee17
Divetrainer <ian@divelab.tech>
2025-12-03 21:59:26 -0800

    part 1 completed

--- day2/day2.py
+++ day2/day2.py
@@ -6,7 +6,7 @@ def instructions(file_path):
         file_contents = f.read()
     return file_contents
 
-puzzle_input = instructions(test_case).split(",")
+puzzle_input = instructions(file_path).split(",")
 sku_addition = 0
 
 for input in puzzle_input:
@@ -14,5 +14,13 @@ for input in puzzle_input:
     low_range = int(hi_lo_options[0])
     high_range = int(hi_lo_options[1])
     for num in range(low_range, high_range+1):
-        if num == 0:
-            pass
+        num_str = str(num)
+        
+        if not len(num_str) % 2:
+            split_by = len(num_str)/2
+            first_half = num_str[:int(split_by)]
+            second_half = num_str[int(split_by):]
+            if int(first_half) == int(second_half):
+                sku_addition += num
+
+print(sku_addition)
